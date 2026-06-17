a6510342eb4f3941f79019d6a3de31853147b045
thefamilypantryllc <thefamilypantryllc@gmail.com>
2026-06-17 11:39:19 -0500

    Fix Square access token configuration

--- server/square.js
+++ server/square.js
@@ -3,9 +3,7 @@ const { SquareClient, SquareEnvironment } = require('square');
 const { isProduction, SQUARE_ACCESS_TOKEN } = require('./config');
 
 const client = new SquareClient({
-  token:
-    process.env.SQUARE_ACCESS_TOKEN ||
-    process.env.SQUARE_TOKEN,
+  token: process.env.SQUARE_ACCESS_TOKEN || process.env.SQUARE_TOKEN,
   environment: SquareEnvironment.Sandbox,
 });
 
